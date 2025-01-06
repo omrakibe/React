9db4fc2aa6070c38062adddb7b474eb4734061f4
omrakibe <rakibeom30@gmail.com>
2025-01-06 14:05:45 +0530

    changes directory

---
+++ 9. Forms in React/1. Inputs  and label/src/Form.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+function Form() {
+  let [firstName, setFirstName] = useState("");
+  // console.log(firstName);
+
+  let [lastName, setLastName] = useState("");
+  // console.log(firstName + " " + lastName);
+
+  let handleFirstName = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  let handleLastName = (event) => {
+    setLastName(event.target.value);
+  };
+
+  return (
+    <>
+      <h1>Form</h1>
+      <form action="">
+        <label htmlFor="firstName">Full Name - </label>
+        <input
+          id="firstName"
+          type="text"
+          placeholder="enter First Name here"
+          value={firstName}
+          onChange={handleFirstName}
+        />
+        &nbsp; &nbsp;
+        <label htmlFor="lastName">Last Name - </label>
+        <input
+          id="enter last name here"
+          type="text"
+          placeholder="Hello"
+          value={lastName}
+          onChange={handleLastName}
+        />
+        <button>Submit</button>
+      </form>
+    </>
+  );
+}
+
+export default Form;
